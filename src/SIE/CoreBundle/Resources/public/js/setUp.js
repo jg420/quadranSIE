fbf9168fa5aeee3f524f05ca9f8e228e081cb24f
g.juillerot <g.juillerot@P702-288-GJU.QUADRAN.LAN>
2017-03-20 04:35:13 +0100

    MAJ path serveur dans js

--- src/SIE/CoreBundle/Resources/public/js/setUp.js
+++ src/SIE/CoreBundle/Resources/public/js/setUp.js
@@ -15,7 +15,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var hostPath_dev="http://127.0.0.1/web/app_dev.php/";
-var hostPath_prod="http://127.0.0.1/web/app.php/";
-var uploadPath_dev="http://127.0.0.1/web/";
-var uploadPath_prod="http://127.0.0.1/web/app.php/";
+//var hostPath_dev="http://127.0.0.1/web/app_dev.php/";
+var hostPath_dev="http://192.168.4.250/web/app_dev.php/";
+
+//var hostPath_prod="http://127.0.0.1/web/app.php/";
+var hostPath_prod="http://192.168.4.250/web/app.php/";
+
+//var uploadPath_dev="http://127.0.0.1/web/";
+var uploadPath_dev="http://192.168.4.250/web/";
+
+//var uploadPath_prod="http://127.0.0.1/web/app.php/";
+var uploadPath_prod="http://192.168.4.250/web/app.php/";
