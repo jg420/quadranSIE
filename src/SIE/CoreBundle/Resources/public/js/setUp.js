429450a73ac882f38ff651a1ca40753f4a46b623
g.juillerot <g.juillerot@P702-288-GJU.QUADRAN.LAN>
2017-03-27 00:47:41 +0200

    Revert "MAJ  gju"
    This reverts commit 4723b16c826b4e9719838ed46618dbd4dff64513.

--- src/SIE/CoreBundle/Resources/public/js/setUp.js
+++ src/SIE/CoreBundle/Resources/public/js/setUp.js
@@ -15,16 +15,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//var host="http://127.0.0.1/";
-var host="http://192.168.4.250/";
-
 //var hostPath_dev="http://127.0.0.1/web/app_dev.php/";
-var hostPath_dev=host+"web/app_dev.php/";
+var hostPath_dev="http://192.168.4.250/web/app_dev.php/";
 
 //var hostPath_prod="http://127.0.0.1/web/app.php/";
-var hostPath_prod=host+"web/app.php/";
+var hostPath_prod="http://192.168.4.250/web/app.php/";
 
 //var uploadPath_dev="http://127.0.0.1/web/";
-var uploadPath_dev=host+"web/";
+var uploadPath_dev="http://192.168.4.250/web/";
 
- 
+//var uploadPath_prod="http://127.0.0.1/web/app.php/";
+var uploadPath_prod="http://192.168.4.250/web/app.php/";
